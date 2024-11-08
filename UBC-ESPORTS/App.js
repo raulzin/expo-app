e746db53d7593ff4003b73ee3433faa0780868af
raulzin <raulsanttos293@gmail.com>
2024-11-07 22:25:44 -0300

    Git Push

--- UBC-ESPORTS/App.js
+++ UBC-ESPORTS/App.js
@@ -4,8 +4,11 @@ import { TouchableOpacity, StyleSheet, Text, View, Image } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+      source={require('./C:\Users\User\OneDrive\Documentos\expo-app\UBC-ESPORTS\.gitignore')}
+      style={styles.Image}/>
+      <Text>Bem Vindo ao UBC ESPORTS!</Text>
+      <StatusBar styles="auto" />
     </View>
   );
 }
