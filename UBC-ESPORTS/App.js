b95dad308cf7aa53f49fa9c561e6c7c8a37ff987
raulzin <raulsanttos293@gmail.com>
2024-11-09 22:24:54 -0300

    Git Push

--- UBC-ESPORTS/App.js
+++ UBC-ESPORTS/App.js
@@ -7,6 +7,9 @@ export default function App() {
       <Image
       source={require('./src/components/football.png')}
       style={styles.Image}/>
+       <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Pressione-me</Text>
+      </TouchableOpacity>
       <Text>Bem Vindo ao UBC ESPORTS!</Text>
       <StatusBar styles="auto" />
     </View>
@@ -19,5 +22,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: '#6200ee',
+    paddingVertical: 10,  // Ajuste do padding vertical (reduz a altura)
+    paddingHorizontal: 20, // Ajuste do padding horizontal (reduz a largura)
+    borderRadius: 5,       // Borda arredondada do botão
+    width: 150,            // Ajuste da largura do botão
+    height: 40,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 14,          // Tamanho do texto dentro do botão
+    textAlign: 'center',
   },
 });
